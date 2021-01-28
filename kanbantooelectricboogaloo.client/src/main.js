7f7c0c35335a6cb97ae9cec167312e02eb213c85
Michelle Charron <michelle.charron93@gmail.com>
2021-01-28 14:42:10 -0700

    importing corejs and others to use modal toggles

--- kanbantooelectricboogaloo.client/src/main.js
+++ kanbantooelectricboogaloo.client/src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import { createApp } from 'vue'
 import { registerGlobalComponents } from './registerGlobalComponents'
 import router from './router'
+import bootstrap from 'bootstrap'
+import jquery from 'jquery'
+import popper from 'popper.js'
+import corejs from 'core-js'
 
 const root = createApp(App)
 registerGlobalComponents(root)
 
 root
-  .use(router)
+  .use(router, bootstrap, jquery, popper, corejs)
   .mount('#app')
